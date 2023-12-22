2bd6dcd5c5d6c0cbd3d22f471166d9fe007d71c8
Rk344300 <rk344300@gmail.com>
2023-12-22 12:51:41 +0530

    first commit

---
+++ src/dataFile.js
@@ -0,0 +1,67 @@
+
+export const apartmanetDetails = {
+    banner: {
+      img_url:
+        "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/f25a4910-ae72-47d1-a77a-1c550ab57626.png",
+      heading: "Seaton Ave",
+      subheading: "Fridai Apartment",
+      desc: "“Your Peaceful Retreat”",
+      rating: 4.8,
+      review: 241,
+      location: "Alexandria, Virginia",
+      para: "For travelers with a peaceful getaway for extended stays of 30 days or more. Experience the perfect blend of comfort, convenience, and serenity in a space designed to inspire and relax.",
+      price: "$4,600.00",
+      button:"Talk with your Home Girl",
+    },
+    neighbourHood: {
+      grade: "A+",
+      desc: "Fridai Apartment is situated in a thriving community that offers safety, convenience, and a vibrant atmosphere.",
+      safety:"A+",
+      culturalDiversity:"A+",
+      educationAndSchools:"A+",
+      convenience:"A+",
+      healthcareFacilities:"A+",
+      scenicViews:"A+"
+    },
+    apartmentDetails :{
+      guest:"2-3 Guests",
+      bedroom:"1 Bedroom",
+      bath:"1 Private Bath",
+      days:"30 days/more",
+      desc:`Discover a tranquil escape in the heart of Alexandria, Virginia. The Fridai Apartment offers more than just accommodation; it's your home away from home for extended stays. 
+  
+      Experience the perfect blend of comfort, convenience, and serenity in a space designed to inspire and relax.`
+    },
+    apartmentFeatures:{
+      modernComforts:"Our spacious apartments offer modern amenities for your comfort. Enjoy fully-equipped kitchens and cozy living areas for a seamless stay.",
+      scenicView:"Scenic Views Wake up to city vistas or tranquil surroundings. Our apartments provide a picturesque backdrop for your moments of reflection.",
+      peacefulAmbinace:"Peaceful Ambiance Relax in a meticulously designed, tranquil atmosphere. The Fridai Apartment is your haven for work, relaxation, and inner peace."
+    },
+    extendedPerks:{
+      stays:"Flexible Stays Whether you're planning a month-long retreat or need an extended stay for work, our flexible booking options cater to your needs.",
+      dining:"Dining Convenience Enjoy dining options within walking distance or take advantage of your fully-equipped kitchen to whip up your favorite meals.",
+      wifi:"Complimentary Wi-Fi Stay connected with complimentary high-speed Wi-Fi available throughout the apartment.",
+      desc:`Nestled in Alexandria's heart, the Fridai Apartment offers easy access to the historic Old Town. 
+      Wander through cobblestone streets, explore charming shops, and savor moments at cozy cafes.
+      
+      With public transportation at your fingertips, commuting is a breeze. 
+      Dive into Alexandria's vibrant arts and culture scene, making the most of your stay's central location.`
+    },
+    gallery:[
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/1b605f68-6c2e-4fec-bb08-d578b2424c60.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/836042d5-e08b-485a-8df1-65d592a930db.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/164dd177-57c4-4b6b-966a-e621cefdd7df.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/4fa4c711-0080-4bdd-a876-7219ef121adc.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/8d8bd942-1b3e-4bcf-98d0-53e8683ce293.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/6f6d4981-0f92-42b9-b652-f38fefc9036f.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/f76099b2-44be-45b6-a505-4bc0f7ee3f89.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/a97e2657-3aa8-4d91-a6e9-eadcf85e524d.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/2cd24cdb-4143-4a08-b4fe-69939c1fc72f.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/adc08ad8-b139-44e3-b126-06444c999054.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/1af1e1cf-9a7b-4966-beb4-a3d106c55b59.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/48e65d8b-11d5-481d-8123-5b71168dd498.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/2599a95c-657e-43d1-b8d5-2af40fb3d689.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/599ec5f7-1eaa-4de9-9d2d-e8431cef97f3.png",
+      "https://s3.ap-south-1.amazonaws.com/cdn.ghc.health/0e0a7b11-156a-433b-8ef4-52e80419ca67.png"
+  ]
+  };
